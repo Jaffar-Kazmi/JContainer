96bf887a41293787f322763bffad0f4abfea2d60
Jaffar kazmi <kazmijaffar890@gmail.com>
2025-11-20 01:39:39 +0500

    feat(cgroup): PID tracking & limit enforcement

--- main.go
+++ main.go
@@ -4,57 +4,106 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
 	"syscall"
 )
 
-// docker           run image <command> <params>
-// go run main.go	run		  <command> <params>
-// pwd: /home/jaffar/Documents/Lectures/OSLabs/Project
-// root directory for container: /home/jaffar/Documents/Lectures/OSLabs/Project/jroot/
-
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go run <command> [args...]")
+		os.Exit(1)
+	}
+
 	switch os.Args[1] {
 	case "run":
 		run()
 	case "child":
 		child()
-
 	default:
-		panic("bad command")
+		panic("Bad command. Use 'run' or 'child'")
 	}
 }
 
 func run() {
-	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
+	fmt.Printf("Parent: Running %v as PID %d\n", os.Args[2:], os.Getpid())
 
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
+	// Create cgroup BEFORE starting the process
+	cgroupPath := setupCgroup()
+
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
-		Unshareflags: syscall.CLONE_NEWNS,
+		Cloneflags: syscall.CLONE_NEWUTS |
+			syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNS,
 	}
 
-	cmd.Run()
+	// Start the process (don't wait yet)
+	must(cmd.Start())
+
+	fmt.Printf("Parent: Started child process, waiting for completion\n")
+
+	// Now wait for it to finish
+	must(cmd.Wait())
+
+	// Cleanup cgroup
+	cleanupCgroup(cgroupPath)
 }
 
 func child() {
-	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
+	fmt.Printf("Child: Running %v as PID %d\n", os.Args[2:], os.Getpid())
 
+	pid := os.Getpid()
+	cgroupPath := "/sys/fs/cgroup/jcontainer"
+
+	// Write our PID to the cgroup
+	must(os.WriteFile(cgroupPath+"/cgroup.procs", []byte(strconv.Itoa(pid)), 0644))
+	fmt.Printf("Child: Added self (PID %d) to cgroup\n", pid)
+
+	// Set hostname in new UTS namespace
 	must(syscall.Sethostname([]byte("jcontainer")))
+
+	// Change root filesystem
 	must(syscall.Chroot("/home/jaffar/Documents/Lectures/OSLabs/Project/jroot/"))
 	must(syscall.Chdir("/"))
+
+	// Mount proc filesystem
 	must(syscall.Mount("proc", "proc", "proc", 0, ""))
 
+	// Run the actual command
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	must(cmd.Run())
-	must(syscall.Unmount("/proc", 0))
 
+	// Cleanup
+	must(syscall.Unmount("/proc", 0))
+}
+
+func setupCgroup() string {
+	cgroupPath := "/sys/fs/cgroup/jcontainer"
+
+	os.RemoveAll(cgroupPath)
+
+	// Create cgroup directory
+	must(os.MkdirAll(cgroupPath, 0755))
+	fmt.Println("Created cgroup:", cgroupPath)
+
+	// Set PID limit to 10 processes
+	must(os.WriteFile(cgroupPath+"/pids.max", []byte("10"), 0644))
+
+	return cgroupPath
+}
+
+func cleanupCgroup(cgroupPath string) {
+	// Remove the cgroup directory
+	os.Remove(cgroupPath)
+	fmt.Println("Cleaned up cgroup:", cgroupPath)
 }
 
 func must(err error) {
